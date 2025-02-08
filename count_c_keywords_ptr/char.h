d7df30c6b5c151e7ef17de9d0eac477c6a20938c
uny <2493678688@qq.com>
2025-02-08 16:36:28 +0800

    update

---
+++ count_c_keywords_ptr/char.h
@@ -0,0 +1,21 @@
+#ifndef LEARN_C_CHAR_H
+#define LEARN_C_CHAR_H
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE) {
+        printf("ungetch: too many characters\n");
+    } else {
+        buf[bufp++] = c;
+    }
+}
+
+#endif //LEARN_C_CHAR_H
